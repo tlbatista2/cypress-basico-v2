691dfd3ea63b744d75e580c5bfa36a7e1ec234fa
C1322309 Tiago Lira Batista <c1322309@interno.bb.com.br>
2024-10-06 12:22:57 -0300

    preenchendo campos obrigatórios e enviando formulário

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,21 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
+ beforeEach(function() {
+    cy.visit('./src/index.html')
+  })
+
   it('verifica o título da aplicação', function() {
     cy.visit('./src/index.html')
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it.only('preenche os campos obrigatórios e envia formulário', function() {
+    cy.get('#firstName').type('Tiago')
+    cy.get('#lastName').type('Lira')
+    cy.get('#email').type('tlbatista2@gmail.com')
+    cy.get('#open-text-area').type('Teste')
+    cy.get('button[type="submit"]').click()
+    cy.get('.success').should('be.visible')
   })
 })
