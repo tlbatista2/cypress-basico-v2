f0b32dfd6035e8b27aefb7a75de4c9be215c17d2
C1322309 Tiago Lira Batista <c1322309@interno.bb.com.br>
2024-10-16 10:36:33 -0300

    marca cada tipo de atendimento

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -107,11 +107,19 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       .should('have.value', 'blog')
   })
 
-  it.only('marca o tipo de atendimento "Feedback ', function() {
+  it('marca o tipo de atendimento "Feedback ', function() {
     cy.get('input[type="radio"][value="feedback"]')
       .check()
       .should('have.value', 'feedback')
+  })
 
+  it.only('marca cada tipo de atendimento', function() {
+    cy.get('input[type="radio"]')
+      .should('have.length', 3)
+      .each(function($radio){
+        cy.wrap($radio).check()
+        cy.wrap($radio).should('be.checked')
+      })
   })
 
 })
